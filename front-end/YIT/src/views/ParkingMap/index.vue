bd256438b18a7d35bfa3efc780d142a8b0ebc20c
TW <2547815966@qq.com>
2024-11-01 15:34:54 +0800

    给定车位编号和对应状态，直接生成地图

--- front-end/YIT/src/views/ParkingMap/index.vue
+++ front-end/YIT/src/views/ParkingMap/index.vue
@@ -1,189 +1,130 @@
 <template>
     <div class="map">
-        <div class="row">
-            <div 
-                v-for="(space, index) in row1"
-                :key="index"
-                class="space"
-                :class="{occupied: space.occupied, free: !space.occupied}"
-            >
-                {{ space.parkId }}
-            </div>
-        </div>
-        <div class="row">
-            <div 
-                v-for="(space, index) in row2"
-                :key="index"
-                class="space"
-                :class="{occupied: space.occupied, free: !space.occupied}"
-            >
-                {{ space.parkId }}
-            </div>
-        </div>
-        <div class="row">
-            <div 
-                v-for="(space, index) in row3"
-                :key="index"
-                class="space"
-                :class="{occupied: space.occupied, free: !space.occupied}"
-            >
-                {{ space.parkId }}
-            </div>
-        </div>
+      <ParkingMap :parkingSlots="parkingSlots" />
     </div>
-</template>
-
-<script setup>
-    import { onMounted, ref } from 'vue';
-    import { getMap } from '@/utils/api'
-    const row1 = ref([
-    // {
-    //         parkId: 1,
-    //         occupied: true,
-    //         userId: 1,
-    //         carId: 'a'
-    //     },
-    //     {
-    //         parkId: 2,
-    //         occupied: false,
-    //         userId: 1,
-    //         carId: 'a'
-    //     },
-    //     {
-    //         parkId: 3,
-    //         occupied: true,
-    //         userId: 1,
-    //         carId: 'a'
-    //     },
-    //     {
-    //         parkId: 4,
-    //         occupied: true,
-    //         userId: 1,
-    //         carId: 'a'
-    //     },
-    //     {
-    //         parkId: 5,
-    //         occupied: true,
-    //         userId: 1,
-    //         carId: 'a'
-    //     }
-    ])
-    const row2 = ref([
-    // {
-    //         parkId: 6,
-    //         occupied: true,
-    //         userId: 1,
-    //         carId: 'a'
-    //     },
-    //     {
-    //         parkId: 7,
-    //         occupied: true,
-    //         userId: 1,
-    //         carId: 'a'
-    //     },
-    //     {
-    //         parkId: 8,
-    //         occupied: false,
-    //         userId: 1,
-    //         carId: 'a'
-    //     },
-    //     {
-    //         parkId: 9,
-    //         occupied: false,
-    //         userId: 1,
-    //         carId: 'a'
-    //     },
-    //     {
-    //         parkId: 10,
-    //         occupied: true,
-    //         userId: 1,
-    //         carId: 'a'
-    //     }
-    ])
-    const row3 = ref([
-    // {
-    //         parkId: 11,
-    //         occupied: false,
-    //         userId: 1,
-    //         carId: 'a'
-    //     },
-    //     {
-    //         parkId: 12,
-    //         occupied: true,
-    //         userId: 1,
-    //         carId: 'a'
-    //     },
-    //     {
-    //         parkId: 13,
-    //         occupied: true,
-    //         userId: 1,
-    //         carId: 'a'
-    //     },
-    //     {
-    //         parkId: 14,
-    //         occupied: false,
-    //         userId: 1,
-    //         carId: 'a'
-    //     },
-    //     {
-    //         parkId: 15,
-    //         occupied: false,
-    //         userId: 1,
-    //         carId: 'a'
-    //     }
-    ])
-
-    const fetchMap = async () => {
-        console.log('fetchMap')
-        try {
-            const response = await getMap()
-            row1.value = response.slice(0, 5)
-            row2.value = response.slice(5, 10)
-            row3.value = response.slice(10, 15)
-        } catch (error) {
-            console.log('fetchMap() failed')
-        }
-    }
-
-    onMounted(() => {
-        fetchMap()
-    })
-</script>
-
-<style scoped>
-    .map {
+  </template>
+  
+  <script>
+  import ParkingMap from "./components/ParkingMap.vue";
+  
+  export default {
+    components: {
+      ParkingMap,
+    },
+    data() {
+      return {
+        // 示例数据：车位编号和占用状态
+        parkingSlots: [
+      { slotNumber: 1, occupied: 1 },
+      { slotNumber: 3, occupied: 0 },
+      { slotNumber: 5, occupied: 1 },
+      { slotNumber: 7, occupied: 0 },
+      { slotNumber: 9, occupied: 1 },
+      { slotNumber: 11, occupied: 1 },
+      { slotNumber: 13, occupied: 0 },
+      { slotNumber: 15, occupied: 1 },
+      { slotNumber: 17, occupied: 0 },
+      { slotNumber: 19, occupied: 1 },
+      { slotNumber: 21, occupied: 1 },
+      { slotNumber: 23, occupied: 0 },
+      { slotNumber: 25, occupied: 1 },
+      { slotNumber: 27, occupied: 0 },
+      { slotNumber: 29, occupied: 1 },
+      { slotNumber: 31, occupied: 0 },
+      { slotNumber: 37, occupied: 0 },
+      { slotNumber: 39, occupied: 0 },
+      { slotNumber: 41, occupied: 0 },
+      { slotNumber: 43, occupied: 0 },
+      { slotNumber: 45, occupied: 0 },
+      { slotNumber: 47, occupied: 0 },
+      { slotNumber: 49, occupied: 0 },
+      { slotNumber: 351, occupied: 1 },
+      { slotNumber: 353, occupied: 1 },
+      { slotNumber: 355, occupied: 1 },
+      { slotNumber: 357, occupied: 0 },
+      { slotNumber: 359, occupied: 1 },
+      { slotNumber: 361, occupied: 0 },
+      { slotNumber: 363, occupied: 1 },
+      { slotNumber: 369, occupied: 0 },
+      { slotNumber: 371, occupied: 1 },
+      { slotNumber: 373, occupied: 1 },
+      { slotNumber: 375, occupied: 1 },
+      { slotNumber: 377, occupied: 0 },
+      { slotNumber: 379, occupied: 1 },
+      { slotNumber: 381, occupied: 0 },
+      { slotNumber: 383, occupied: 0 },
+      { slotNumber: 385, occupied: 0 },
+      { slotNumber: 387, occupied: 0 },
+      { slotNumber: 389, occupied: 0 },
+      { slotNumber: 391, occupied: 0 },
+      { slotNumber: 393, occupied: 1 },
+      { slotNumber: 395, occupied: 1 },
+      { slotNumber: 397, occupied: 0 },
+      { slotNumber: 399, occupied: 1 },
+      { slotNumber: 551, occupied: 1 },
+      { slotNumber: 553, occupied: 0 },
+      { slotNumber: 555, occupied: 1 },
+      { slotNumber: 557, occupied: 1 },
+      { slotNumber: 559, occupied: 1 },
+      { slotNumber: 561, occupied: 1 },
+      { slotNumber: 563, occupied: 1 },
+      { slotNumber: 569, occupied: 0 },
+      { slotNumber: 571, occupied: 1 },
+      { slotNumber: 573, occupied: 1 },
+      { slotNumber: 575, occupied: 1 },
+      { slotNumber: 577, occupied: 0 },
+      { slotNumber: 579, occupied: 1 },
+      { slotNumber: 581, occupied: 0 },
+      { slotNumber: 583, occupied: 0 },
+      { slotNumber: 585, occupied: 1 },
+      { slotNumber: 587, occupied: 0 },
+      { slotNumber: 589, occupied: 1 },
+      { slotNumber: 591, occupied: 0 },
+      { slotNumber: 593, occupied: 1 },
+      { slotNumber: 595, occupied: 1 },
+      { slotNumber: 597, occupied: 0 },
+      { slotNumber: 599, occupied: 0 },
+      { slotNumber: 901, occupied: 1 },
+      { slotNumber: 903, occupied: 1 },
+      { slotNumber: 905, occupied: 1 },
+      { slotNumber: 907, occupied: 1 },
+      { slotNumber: 909, occupied: 1 },
+      { slotNumber: 911, occupied: 1 },
+      { slotNumber: 913, occupied: 1 },
+      { slotNumber: 915, occupied: 1 },
+      { slotNumber: 917, occupied: 1 },
+      { slotNumber: 919, occupied: 1 },
+      { slotNumber: 921, occupied: 1 },
+      { slotNumber: 923, occupied: 1 },
+      { slotNumber: 925, occupied: 1 },
+      { slotNumber: 931, occupied: 1 },
+      { slotNumber: 933, occupied: 1 },
+      { slotNumber: 935, occupied: 1 },
+      { slotNumber: 937, occupied: 1 },
+      { slotNumber: 939, occupied: 1 },
+      { slotNumber: 941, occupied: 1 },
+      { slotNumber: 943, occupied: 1 },
+      { slotNumber: 945, occupied: 1 },
+      { slotNumber: 947, occupied: 1 },
+      { slotNumber: 949, occupied: 1 },
+      { slotNumber: 2103, occupied: 0 },
+      { slotNumber: 2109, occupied: 1 },
+      { slotNumber: 2115, occupied: 0 },
+      { slotNumber: 2121, occupied: 1 },
+      // 继续添加更多车位数据，尽量覆盖不同区域
+    ],
+      };
+    },
+  };
+  </script>
+  
+  <style>
+.map {
         display: flex;
         flex-direction: column;
         justify-content: center;
-        height: calc(100vh - 51px);
+        height: calc(100vh - 0px);
         width: calc(100vw - 181px);
     }
-
-    .row {
-        display: flex;
-        /* height: 150px; */
-        justify-content: center;
-        align-items: center;
-    }
-
-    .row .space {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        margin: 20px;
-        height: 200px;
-        width: 120px;
-        color: white;
-        font-weight: 800;
-        font-size: 40px;
-    }
-
-    .occupied {
-        background-color: rgb(192, 15, 15);
-    }
-
-    .free {
-        background-color: rgb(9, 112, 9);
-    }
-
-</style>
+  </style>
+  
