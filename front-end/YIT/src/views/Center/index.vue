6ec4a353bbadb4778238fea307d34cb6be6c4ac0
fromostart <125817180+fromostart@users.noreply.github.com>
2024-11-03 20:13:42 +0800

    增加了用户移动端登录界面与简陋的首页

---
+++ front-end/YIT/src/views/Center/index.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="usernav">
+
+          <span slot="title">我的主页1</span>
+    </div>
+    <RouterView />
+</template>
+  
+<script lang="ts" setup>
+  import {
+    Document,
+    Menu as IconMenu,
+    VideoCameraFilled,
+    Location,
+    Setting,
+  } from '@element-plus/icons-vue'
+  import {ref} from 'vue';
+  const currentActive = ref(window.location.pathname);
+</script>
+
+<style scoped>
+    .usernav {
+        float: left;
+        width: 180px;
+        height: calc(100vh - 51px);
+        border-right: 1px solid #dcdfe6;
+    }
+
+    .usernav .el-menu {
+        border: none;
+    }
+
+    a {
+      text-decoration: none;
+    }
+</style>
